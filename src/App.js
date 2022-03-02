5f12d73160a274c6654434103180707174b611ba
namlh <hoainamle023@gmail.com>
2022-03-02 23:16:09 +0700

    Fix main max-width

--- src/App.js
+++ src/App.js
@@ -48,76 +48,78 @@ function App() {
 
   return (
     <div className="container" data-theme={theme}>
-      <header className="header">
-        <div className="header__logo">devfinder</div>
-        <div className="header__mode" onClick={switchTheme}>
-          <p>{theme === "dark" ? "LIGHT" : "DARK"}</p>
-          <img
-            src={theme === "dark" ? iconSun : iconMoon}
-            alt=""
-            aria-hidden="true"
-          />
-        </div>
-      </header>
-      <main className="main">
-        <div className="search">
-          <img src={iconSearch} alt="" aria-hidden="true" />
-          <input
-            placeholder="Search GitHub username..."
-            type="text"
-            id="search-input"
-          />
-          <button>Search</button>
-        </div>
-        <div className="content">
-          <div className="avatar">
-            <img src="" alt="" aria-hidden="true" />
+      <div className="wrapper">
+        <header className="header">
+          <div className="header__logo">devfinder</div>
+          <div className="header__mode" onClick={switchTheme}>
+            <p>{theme === "dark" ? "LIGHT" : "DARK"}</p>
+            <img
+              src={theme === "dark" ? iconSun : iconMoon}
+              alt=""
+              aria-hidden="true"
+            />
           </div>
-          <div className="user-general">
-            <h1 className="h1-like">The Octocat</h1>
-            <p className="username">@octocat</p>
-            <p className="date-join">Joined 25 Jan 2011</p>
+        </header>
+        <main className="main">
+          <div className="search">
+            <img src={iconSearch} alt="" aria-hidden="true" />
+            <input
+              placeholder="Search GitHub username..."
+              type="text"
+              id="search-input"
+            />
+            <button>Search</button>
           </div>
-          <div className="user-detail">
-            <p className="des">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
-              odio. Quisque volutpat mattis eros.
-            </p>
-            <ul className="stats">
-              <li className="stat">
-                <p>Repos</p>
-                <p>8</p>
-              </li>
-              <li className="stat">
-                <p>Followers</p>
-                <p>39883</p>
-              </li>
-              <li className="stat">
-                <p>Following</p>
-                <p>8</p>
-              </li>
-            </ul>
-            <ul className="address">
-              <li>
-                <img src={iconLocation} alt="" aria-hidden="true" />
-                <p>San Francisco</p>
-              </li>
-              <li>
-                <img src={iconWebsite} alt="" aria-hidden="true" />
-                <p>https://github.blog</p>
-              </li>
-              <li>
-                <img src={iconTwitter} alt="" aria-hidden="true" />
-                <p>Not Available</p>
-              </li>
-              <li>
-                <img src={iconCompany} alt="" aria-hidden="true" />
-                <p>@github</p>
-              </li>
-            </ul>
+          <div className="content">
+            <div className="avatar">
+              <img src="" alt="" aria-hidden="true" />
+            </div>
+            <div className="user-general">
+              <h1 className="h1-like">The Octocat</h1>
+              <p className="username">@octocat</p>
+              <p className="date-join">Joined 25 Jan 2011</p>
+            </div>
+            <div className="user-detail">
+              <p className="des">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+                odio. Quisque volutpat mattis eros.
+              </p>
+              <ul className="stats">
+                <li className="stat">
+                  <p>Repos</p>
+                  <p>8</p>
+                </li>
+                <li className="stat">
+                  <p>Followers</p>
+                  <p>39883</p>
+                </li>
+                <li className="stat">
+                  <p>Following</p>
+                  <p>8</p>
+                </li>
+              </ul>
+              <ul className="address">
+                <li>
+                  <img src={iconLocation} alt="" aria-hidden="true" />
+                  <p>San Francisco</p>
+                </li>
+                <li>
+                  <img src={iconWebsite} alt="" aria-hidden="true" />
+                  <p>https://github.blog</p>
+                </li>
+                <li>
+                  <img src={iconTwitter} alt="" aria-hidden="true" />
+                  <p>Not Available</p>
+                </li>
+                <li>
+                  <img src={iconCompany} alt="" aria-hidden="true" />
+                  <p>@github</p>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 }
