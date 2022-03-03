d1b317b378b49bb26506d2992933450df10e441b
namlh <hoainamle023@gmail.com>
2022-03-03 17:17:06 +0700

    Add search github-user feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
+import classNames from "classnames";
+
 import "./assets/scss/main.scss";
 import iconMoon from "./assets/images/icon-moon.svg";
 import iconSun from "./assets/images/icon-sun.svg";
@@ -9,8 +12,48 @@ import iconTwitter from "./assets/images/icon-twitter.svg";
 import iconWebsite from "./assets/images/icon-website.svg";
 
 function App() {
-  const [iconMode, setIconMode] = useState(iconSun);
   const [theme, setTheme] = useState("light");
+  const [username, setUsername] = useState("");
+  const [isUserNotFound, setIsUserNotFound] = useState(false);
+  const [user, setUser] = useState({
+    login: "batman",
+    name: "The Batman",
+    company: "The Batman",
+    location: "Gotham",
+    blog: "https://thebatman.com",
+    bio: "The man with a bat",
+    twitter_username: "TheBatman",
+    avatar_url: null,
+    public_repos: 0,
+    followers: 0,
+    following: 0,
+    created_at: "2011-01-25T18:44:36Z",
+  });
+
+  const NOT_AVAILABLE = "Not Available";
+  const API_URL = "https://api.github.com/users/";
+  const OCTOCAT = "octocat";
+
+  async function getUsers(username = OCTOCAT) {
+    try {
+      let response = await fetch(API_URL + username);
+      if (response.ok) {
+        let result = await response.json();
+        setUser(result);
+        setIsUserNotFound(false);
+      } else if (response.status === 404) {
+        setIsUserNotFound(true);
+      } else {
+        throw new Error(response.status);
+      }
+    } catch (err) {
+      console.log("ERROR: ", err.message);
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   useEffect(() => {
     function getDefaultTheme() {
@@ -25,9 +68,7 @@ function App() {
 
       if (localStorage.getItem("theme") === "dark") {
         theme = "dark";
-      }
-
-      if (localStorage.getItem("theme") === "light") {
+      } else {
         theme = "light";
       }
 
@@ -62,58 +103,72 @@ function App() {
         </header>
         <main className="main">
           <div className="search">
+            <p
+              className={classNames("error-text", {
+                "display-error": isUserNotFound,
+              })}
+            >
+              No result
+            </p>
             <img src={iconSearch} alt="" aria-hidden="true" />
             <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               placeholder="Search GitHub username..."
               type="text"
               id="search-input"
             />
-            <button>Search</button>
+            <button onClick={() => getUsers(username)}>Search</button>
           </div>
           <div className="content">
             <div className="avatar">
-              <img src="" alt="" aria-hidden="true" />
+              <img src={user.avatar_url} alt="" aria-hidden="true" />
             </div>
             <div className="user-general">
-              <h1 className="h1-like">The Octocat</h1>
-              <p className="username">@octocat</p>
-              <p className="date-join">Joined 25 Jan 2011</p>
+              <h1 className="h1-like">{user.name || user.login}</h1>
+              <p className="username">@{user.login}</p>
+              <p className="date-join">
+                Joined {moment(user.created_at).format("DD MMM YYYY")}
+              </p>
             </div>
             <div className="user-detail">
-              <p className="des">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
-                odio. Quisque volutpat mattis eros.
+              <p className={classNames("des", { "not-available": !user.bio })}>
+                {user.bio || "This profile has no bio"}
               </p>
               <ul className="stats">
                 <li className="stat">
                   <p>Repos</p>
-                  <p>8</p>
+                  <p>{user.public_repos}</p>
                 </li>
                 <li className="stat">
                   <p>Followers</p>
-                  <p>39883</p>
+                  <p>{user.followers}</p>
                 </li>
                 <li className="stat">
                   <p>Following</p>
-                  <p>8</p>
+                  <p>{user.following}</p>
                 </li>
               </ul>
               <ul className="address">
-                <li>
+                <li className={classNames({ "not-available": !user.location })}>
                   <img src={iconLocation} alt="" aria-hidden="true" />
-                  <p>San Francisco</p>
+                  <p>{user.location || NOT_AVAILABLE}</p>
                 </li>
-                <li>
+                <li className={classNames({ "not-available": !user.blog })}>
                   <img src={iconWebsite} alt="" aria-hidden="true" />
-                  <p>https://github.blog</p>
+                  <p>{user.blog || NOT_AVAILABLE}</p>
                 </li>
-                <li>
+                <li
+                  className={classNames({
+                    "not-available": !user.twitter_username,
+                  })}
+                >
                   <img src={iconTwitter} alt="" aria-hidden="true" />
-                  <p>Not Available</p>
+                  <p>{user.twitter_username || NOT_AVAILABLE}</p>
                 </li>
-                <li>
+                <li className={classNames({ "not-available": !user.company })}>
                   <img src={iconCompany} alt="" aria-hidden="true" />
-                  <p>@github</p>
+                  <p>{user.company || NOT_AVAILABLE}</p>
                 </li>
               </ul>
             </div>
